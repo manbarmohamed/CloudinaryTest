7d4f1e4646e24a43c823b03ac651099268f8c5fa
manbarmohamed <johndoe@example.com>
2025-01-13 15:08:35 +0100

    edit ProductCreateDTO

--- src/main/java/com/example/cloudinary/ProductCreateDTO.java
+++ src/main/java/com/example/cloudinary/ProductCreateDTO.java
@@ -1,5 +1,7 @@
 package com.example.cloudinary;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,12 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ProductCreateDTO {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Description cannot be blank")
     private String description;
+
+    @NotNull(message = "Price cannot be null")
     private Double price;
 }
